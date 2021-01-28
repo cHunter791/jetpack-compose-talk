8017bf6657f80647650c7a19112147266acee5de
Christoper Hunter <chunter791@gmail.com>
2021-01-28 18:28:33 +0000

    Tidying

--- app/src/main/java/com/chunter/composetalk/ui/screens/main/MainActivity.kt
+++ app/src/main/java/com/chunter/composetalk/ui/screens/main/MainActivity.kt
@@ -3,22 +3,4 @@ package com.chunter.composetalk.ui.screens.main
 import androidx.appcompat.app.AppCompatActivity
 import com.chunter.composetalk.R
 
-class MainActivity : AppCompatActivity(R.layout.activity_main) {
-
-//    override fun onBackPressed() {
-//        if (supportFragmentManager.backStackEntryCount > 0) {
-//            supportFragmentManager.popBackStack()
-//        } else {
-//            finish()
-//        }
-//    }
-//
-//    override fun onSupportNavigateUp(): Boolean {
-//        if (supportFragmentManager.backStackEntryCount > 0) {
-//            supportFragmentManager.popBackStack()
-//            return false
-//        }
-//
-//        return super.onSupportNavigateUp()
-//    }
-}
+class MainActivity : AppCompatActivity(R.layout.activity_main)
